f322f2e1720b955206e7d4200db53fe2ec3cd31c
Jean-Pierre Gassin <jean-pierregassin@JeanPierresMBP.lan>
2019-01-20 09:19:20 +1000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+
+namespace DotNetConsole
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            new Program().TestLinq();
+            // new Program().PrintProcessList();
+        }
+
+        private void PrintProcessList()
+        {
+            Process[] processes = Process.GetProcesses();
+
+            foreach (Process process in processes) {
+                Console.WriteLine($"Process: {process.ProcessName} ID: {process.Id}");
+            }
+        }
+
+        private void TestLinq()
+        {
+            int[] numbers = new int[] {0, 1, 2, 3, 4, 5, 6, 7};
+
+            var numberQuery = from num in numbers where (num % 2 == 0) select num;
+
+            foreach (int number in numberQuery) {
+                Console.WriteLine($"{number} is divisible by 2");
+            }
+        }
+    }
+}
